9ea98b2d8f80a6bafae98c613cc0cb5b4541f0d0
EPrgrm <leonellusxd@gmail.com>
2024-08-12 16:14:29 +0300

    config.sh

--- config.sh
+++ config.sh
@@ -28,7 +28,7 @@ BOOTTYPE="pd-ssd"
 PREEMPTIBLE="true"
 
 # GCP Base Image and Family (replace these if you're using your own custom image - give it a family name and enter the Project ID)
-IMAGEBASEFAMILY="windows-2019"
+IMAGEBASEFAMILY="windows-2022"
 IMAGEBASEPROJECT="windows-cloud"
 
 # Various resource and label names
